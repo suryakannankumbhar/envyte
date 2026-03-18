961a35125245f235bea4f9fd2924d1d920c86dbb
suryakannankumbhar <suryakannankumbhar@gmail.com>
2026-03-18 09:51:02 +0530

    deployment settings update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAt7FPtAygtZp0tMU9Bt-VR-nRHEIsI1l4',
-    authDomain: 'envyte-ac907.firebaseapp.com',
-    projectId: 'envyte-ac907',
-    storageBucket: 'envyte-ac907.firebasestorage.app',
-    messagingSenderId: '593833194695',
-    appId: '1:593833194695:web:58609eb548a11d382e7ae2',
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
